2be7ebfb0df91c3b1e5e5f239f9ef10825314527
Shailen Naidoo <shailen@lumkani.com>
2019-11-19 14:09:08 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,9 @@ module.exports = {
       '/',
       '/guide/'
     ],
+    nav: [
+      { text: 'Releases', link: 'https://github.com/Lumkani/vue-view-model-api/releases' }
+    ],
     sidebarDepth: 2,
     displayAllHeaders: true,
     repo: 'Lumkani/vue-view-model-api'
