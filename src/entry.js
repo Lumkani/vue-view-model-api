e50f62defb7a45cf66af7685b4793aec6e0cc90c
shailen <shailen@lumkani.com>
2020-04-01 17:37:03 +0200

    Auto install plugin via CDN

--- src/entry.js
+++ src/entry.js
@@ -1,29 +1,18 @@
 import { ViewModelPlugin } from './plugin'
 
 // install function executed by Vue.use()
-// const install = function installViewModelApi(Vue) {
-//   if (install.installed) return;
-//   install.installed = true;
 
-//   Vue.use(ViewModelPlugin)
-// };
-
-// Create module definition for Vue.use()
-// const plugin = {
-//   install,
-// };
-
-// // To auto-install when vue is found
-// // eslint-disable-next-line no-redeclare
-// /* global window, global */
-// let GlobalVue = null;
-// if (typeof window !== 'undefined') {
-//   GlobalVue = window.Vue;
-// } else if (typeof global !== 'undefined') {
-//   GlobalVue = global.Vue;
-// }
-// if (GlobalVue) {
-//   GlobalVue.use(plugin);
-// }
+// To auto-install when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(ViewModelPlugin);
+}
 
 export { ViewModelPlugin }
