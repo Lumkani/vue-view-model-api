141648d1f339f1b0bb88cd7f02bae09478603e13
Shailen Naidoo <naidooshailen648@gmail.com>
2020-04-04 10:10:42 +0200

    Updated dev.sh

--- dev.sh
+++ dev.sh
@@ -1,5 +1,21 @@
 nvm install
 
+function build_docs {
+  cd docs/
+  pnpx vuepress build
+  cd ..
+}
+
 function run_tests {
-  pnpx jest --verbose
+  pnpx jest --verbose --coverage
+}
+
+function upload_coverage {
+  pnpx codecov
+}
+
+function netlify_pipeline {
+  run_tests
+  upload_coverage
+  build_docs
 }
