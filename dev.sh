ccb0fea00a58d617e366d7c96138a5d83e0380e6
Shailen Naidoo <naidooshailen648@gmail.com>
2020-04-04 12:34:09 +0200

    Minor change

--- dev.sh
+++ dev.sh
@@ -44,5 +44,5 @@ function upload_coverage {
 function netlify_pipeline {
   npm i -g pnpm
 
-  run_lint && run_tests && upload_coverage && build_docs
+  build_package && run_lint && run_tests && upload_coverage && build_docs
 }
